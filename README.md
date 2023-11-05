70d671e77a492e59ec5d0edd120b287382d2154f
bjornlan <149943425+lanttob@users.noreply.github.com>
2023-11-05 16:12:22 +0100

    v

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # learn-terraform-circleci
-
+D
 This is a companion repository for the Hashicorp [Learn content on CircleCI](https://developer.hashicorp.com/terraform/tutorials/automation/circle-ci) tutorial. 
--- main.tf
+++ main.tf
@@ -58,8 +58,7 @@ resource "aws_s3_bucket_website_configuration" "app" {
   }
 }
 
-resource "aws_s3_object" "app" {
-  acl          = "public-read"
+resource "aws_s3_object" "app" 
   key          = "index.html"
   bucket       = aws_s3_bucket.app.id
   content      = file("./assets/index.html")
